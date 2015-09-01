db5daf0b76edde431e86f87e56860d9450d4da9c
Zaid Al Lahham <zlahham@gmail.com>
2015-09-01 12:54:14 +0100

    Create a restaurant feature - done

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150901110140) do
+ActiveRecord::Schema.define(version: 20150901114240) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20150901110140) do
     t.integer  "rating"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.text     "description"
   end
 
 end
