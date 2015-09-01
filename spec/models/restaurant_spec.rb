1414c8fb5423b5e71dd23b62223db0b1c4976638
Rebecca Appleyard <rklappleyard@gmail.com>
2015-09-01 16:40:40 +0100

    Proper testing for review and restaurant dependency

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -1,5 +1,14 @@
 require 'spec_helper'
 
 describe Restaurant, type: :model do
-  it { is_expected.to have_many(:reviews).dependent(:destroy) }
+  it { is_expected.to have_many :reviews }
+
+    describe 'relationship associations' do
+      let!(:kfc){ Restaurant.create(name: 'KFC') }
+      let!(:kfc_review){ Review.create(thoughts: 'so so', rating: 2) }
+      it "has a dependent relationship with reviews" do
+      kfc.reviews << kfc_review
+      expect{ kfc.destroy }.to change{ Review.count }
+    end
+  end
 end
