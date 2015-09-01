315e4c3b452f94ffe2522d45044053e7c481973e
Rebecca Appleyard <rklappleyard@gmail.com>
2015-09-01 17:51:52 +0100

    Added validation on name length of restaurant

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -10,7 +10,11 @@ class RestaurantsController < ApplicationController
 
   def create
     @restaurant = Restaurant.create(restaurant_params)
-    redirect_to '/restaurants'
+    if @restaurant.save
+      redirect_to restaurants_path
+    else
+      render 'new'
+    end
   end
 
   def restaurant_params
@@ -28,14 +32,14 @@ class RestaurantsController < ApplicationController
   def update
     @restaurant = Restaurant.find(params[:id])
     @restaurant.update(restaurant_params)
-    redirect_to '/restaurants'
+    redirect_to restaurants_path
   end
 
   def destroy
     @restaurant = Restaurant.find(params[:id])
     @restaurant.destroy
     flash[:notice] = "Restaurant deleted successfully"
-    redirect_to '/restaurants'
+    redirect_to restaurants_path
   end
 
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,7 @@
 class Restaurant < ActiveRecord::Base
 
   has_many :reviews, dependent: :destroy
+
+  validates :name, length: {minimum: 3}
+
 end
